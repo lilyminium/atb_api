98af55167a4dd4e6a3e6f1672fbbf0112430c423
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-02-18 10:12:16 +1100

    add skipif back

--- atb_api/tests/conftest.py
+++ atb_api/tests/conftest.py
@@ -3,8 +3,9 @@ import pytest
 from atb_api.api import ATBApi
 
 
-# @pytest.mark.skipif("ATB_API_TOKEN" not in os.environ, reason="api token not available")
 @pytest.fixture(scope="session")
 def api():
-    api_ = ATBApi()  # api_token=os.environ["ATB_API_TOKEN"])
+    if "ATB_API_TOKEN" not in os.environ:
+        pytest.skip("api token not available")
+    api_ = ATBApi(api_token=os.environ["ATB_API_TOKEN"])
     return api_
