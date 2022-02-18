139e6ca1572e9e8e74574faf7ad1338dc5123d7a
Lily Wang <31115101+lilyminium@users.noreply.github.com>
2022-02-18 19:03:35 +1100

    update setup.py

--- setup.py
+++ setup.py
@@ -43,15 +43,18 @@ setup(
 
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-
-    # Additional entries you may want simply uncomment the lines you want and fill in the data
-    # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
-    # platforms=['Linux',
-    #            'Mac OS-X',
-    #            'Unix',
-    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
+    install_requires=[
+        "numpy",
+        "pydantic",
+        "pyyaml",
+        "elementable",
+    ],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    platforms=['Linux',
+               'Mac OS-X',
+               'Unix',
+            #    'Windows',
+               ],            # Valid platforms your code works on, adjust to your flavor
+    python_requires=">=3.7",          # Python version restrictions
 
     # Manual control if final package is compressible or not, set False to prevent the .egg from being made
     # zip_safe=False,
